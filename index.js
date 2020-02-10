2684ca6cbd093b88a166e70a4c49c43c2eb71b1e
Connor McGinn <connor.mcginn@daugherty.com>
2020-02-10 16:11:41 -0600

    added routes for handling GET, PUT, DELETE

--- index.js
+++ index.js
@@ -41,6 +41,56 @@ app.post('/beers', (req, res) => {
     });
 });
 
+app.get('/beers', (req, res) => {
+    Beer.find((err, beers) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(beers);
+        };
+    });
+});
+
+// :beer_id in the route needs to match the find by ID req.params.xxxx
+app.get('/beers/:beer_id', (req, res) => {
+    Beer.findById(req.params.beer_id, (err, beer) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(beer);
+        };
+    });
+});
+
+app.put('/beers/:beer_id', (req, res) => {
+    Beer.findById(req.params.beer_id, (err, beer) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            beer.name = req.body.name;
+            beer.rating = req.body.rating;
+            beer.save((err, beer) => {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.send(`Beer posted!\n ${beer}`);
+                };
+            });
+        };
+    });
+});
+
+app.delete('/beers/:beer_id', (req, res) => {
+    Beer.deleteOne({
+        _id: req.params.beer_id
+    }, (err) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.send(`Successfully deleted beer with ID: ${req.params.beer_id}`)
+        };
+    });
+});
 
 // app.use listens for any type of requests
 // app.get listens for only get requests
